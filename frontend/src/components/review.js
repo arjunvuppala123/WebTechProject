60cb0cc37510ff8f4eba444f4b47c2c11a3c7673
arjunvuppala123 <56397988+arjunvuppala123@users.noreply.github.com>
2020-12-07 14:45:20 +0530

    Update review.js

--- frontend/src/components/review.js
+++ frontend/src/components/review.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Article } from './article';
+import { NavigationBar } from './NavigationBar';
 import '../index.css'
 import  MyTaskList from './My-Task-list';
 
 
 
 export const Review = () => ( 
-    <div classname="mainpage"><Article />
+    <div classname="mainpage">
+    <NavigationBar />
+    <Article />
     <hr />
         <MyTaskList />
     </div>
